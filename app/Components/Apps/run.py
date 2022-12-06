5360779234d0f372aea51af414874f7f26bfc891
xAdrianCx <adrian.cotuna26@gmail.com>
2022-12-06 22:27:04 +0200

    Reworked run.py to get attributes from config.ini. Still needs rework

--- app/Components/Apps/run.py
+++ app/Components/Apps/run.py
@@ -1,16 +1,33 @@
 import os
 from app import root_dir    # This returns the absolute path of the application (\"app")
 from configparser import ConfigParser
-from app.template import Template_Config_Handler
+
 
 def make_imports():
     """
     This function adds all needed variables to the Template_Config_Handler class and runs it.
     """
+    # Get the root directory of the app.
     ROOT_DIR = root_dir()
-    cfg_path = os.path.join(ROOT_DIR, "config\\config.ini")
+
+    # Load config file.
+    cfg_path = os.path.join(ROOT_DIR, "config/config.ini")
     cfg = ConfigParser()
     cfg.read(cfg_path)
-    # to continue
+    # Load Template_Config_Handler.
+    tch_path = os.path.join(ROOT_DIR, "template/Template_Config_Handler.py")
+    # Open Template_Config_Handler.py as text.
+    with open(tch_path, "r+") as tch:
+        content = tch.readlines()
+        for line in range(len(content)):
+            if "# Start: Insert Cfg Objects" in content[line]:
+                for section in cfg.sections():
+                    for key, value in cfg.items(section):
+                        content[line + 1] = f"\n\t{key} = {value}\n"
+        # Delete everything from file.
+        tch.seek(0)
+        tch.truncate(0)
+        tch.writelines(content)
+
 
 
