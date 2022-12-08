1fd1e195082885cade4f549ff847698223a538e7
xAdrianCx <adrian.cotuna26@gmail.com>
2022-12-08 20:48:57 +0200

    Reworked run.py to get attributes from config.ini. It gets the class attributes, but it needs rework to create the get methods and config.ini vars.

--- app/Components/Apps/run.py
+++ app/Components/Apps/run.py
@@ -20,32 +20,23 @@ def make_imports():
         cfg_dict[section] = {}
         for key, value in cfg.items(section):
             cfg_dict[section][key] = value
-  
     
-    # Load Template_Config_Handler.
-    tch_path = os.path.join(ROOT_DIR, "template/Template_Config_Handler.py")
     # Open Template_Config_Handler.py as text.
+    tch_path = os.path.join(ROOT_DIR, "template/Template_Config_Handler.py")
     with open(tch_path, "r+") as tch:
         # Get all content from the file
         content = tch.readlines()
         keys_in_cfg = []
-        for line in range(len(content)):
-            if "# Start: Insert Cfg Objects" in content[line]:
-                first_line = line
-                for section in cfg_dict.keys():
-                    # Get the number of keys for each section and add them to the keys_in_cfg list.
-                    keys_in_cfg.append(len(cfg_dict[section]))
-                    for key, value in cfg_dict[section].items():
-                        # Add as many empty rows as the sum of all keys in cfg.ini.
-                        content[line + 1] = "\n" * sum(keys_in_cfg)
-                        # To continue
-                        
-                        
-        
-        # Delete everything from file.
+        for i, line in enumerate(content):
+            if "# Start: Insert Cfg Objects" in line:
+                # Get the total number of keys in each section of .ini file. Add them to keys_in_cfg.
+                for j in cfg_dict.keys():
+                    keys_in_cfg.append(len(cfg_dict[j]))
+                    for key, value in cfg_dict[j].items():
+                        # Add the key-value pairs as class attributes.
+                        content.insert(i + 1, f"\t{key} = {value}\n")
+        # Delete everything from file and go to line 0.
         tch.seek(0)
         tch.truncate(0)
+        # Write everything to file.
         tch.writelines(content)
-
-
-
